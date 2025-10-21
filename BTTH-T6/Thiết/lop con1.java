beed89b4f67efc64face058690f472cd5797234c
vothiet <vothiet031106@gmail.com>
2025-10-21 21:24:30 +0700

    Add files via upload

---
+++ BTTH-T6/Thiết/lop con1.java
@@ -0,0 +1,18 @@
+public class SachGiaoTrinh extends Sach {
+    private String monHoc;
+    private String capDo;
+
+    public SachGiaoTrinh(String maSach, String tieuDe, String tacGia, int namXuatBan, int soLuong,
+                         String monHoc, String capDo) {
+        super(maSach, tieuDe, tacGia, namXuatBan, soLuong);
+        this.monHoc = monHoc;
+        this.capDo = capDo;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                ", Môn học: " + monHoc +
+                ", Cấp độ: " + capDo;
+    }
+}
